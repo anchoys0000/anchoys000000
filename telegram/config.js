5a987014041ac8adcf91a35a8472dda9f1d16026
anchoys0000 <kirillburkalo71@gmail.com>
2026-08-09 17:10:17 +1000

    Delete telegram/config.js

--- telegram/config.js
+++
@@ -1,3 +0,0 @@
-// Единственная точка входа для отправки заказов.
-// Токен Telegram живёт в Cloudflare Worker (Secrets), а не здесь.
-const WORKER_URL = 'https://cafe-orders.kirillburkalo71.workers.dev/';
